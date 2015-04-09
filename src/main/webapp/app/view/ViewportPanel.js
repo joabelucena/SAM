3c73f58a8250c7f63fbb819d6dd2c19005e8a85b
Joabe Lucena <jlucena@ttrans.com.br>
2015-04-09 08:57:47 -0300

    PROD :: New Advanced Interface Features

--- src/main/webapp/app/view/ViewportPanel.js
+++ src/main/webapp/app/view/ViewportPanel.js
@@ -3,6 +3,7 @@ Ext.define('Sam.view.ViewportPanel', {
 	alias : 'widget.viewportpanel',
 	
 	activeTab : 0,
-	id : 'viewportpanel'
+	id : 'viewportpanel',
+	html: '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><marquee behavior=alternate><i><b><h1>SAM - SISTEMA DE APOIO A MANUTENÇÃO</h1></i></b></marquee>'
 		
 });
