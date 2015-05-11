0a5a66cb3dcd828c73dae38938f1c945289710d9
Joabe Lucena <jlucena@ttrans.com.br>
2015-05-11 11:26:08 -0300

    PROD :: ServiceStation - CRUD Operations (BackEnd)

--- src/main/java/br/com/ttrans/samapp/controller/SiteController.java
+++ src/main/java/br/com/ttrans/samapp/controller/SiteController.java
@@ -3,12 +3,19 @@ package br.com.ttrans.samapp.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.ttrans.samapp.model.ServiceStation;
 import br.com.ttrans.samapp.service.SiteService;
+import br.com.ttrans.samapp.service.ServiceStationService;
 
 @Controller
 @RequestMapping("/site")
@@ -16,6 +23,9 @@ public class SiteController {
 	
 	@Autowired
 	private SiteService siteService;
+	
+	@Autowired
+	private ServiceStationService stationService;
 	
 	
 	/*
@@ -31,4 +41,78 @@ public class SiteController {
 		
 		return result;
 	}
+	
+	@RequestMapping("/load/station")
+	@ResponseBody
+	public Map<String, Object> loadStation() {
+		
+		Map<String,Object> result = new HashMap<String, Object>();
+		
+		result.put("data", stationService.loadData());
+		
+		return result;
+	}
+	
+	
+	/*
+	 * CRUD Operations for: ServiceStation
+	 */
+	@RequestMapping("/station/add.action")
+	@ResponseBody
+	public Map<String,Object> addstation(@RequestBody ServiceStation station, 
+			HttpServletRequest request,
+			Authentication authentication,
+            HttpServletResponse response) {
+		
+		//Result Map
+		Map<String,Object> result = new HashMap<String, Object>();
+
+		try{
+			stationService.add(station, authentication);
+		}catch(Exception e){
+			result.put("message",e.getMessage());
+		}
+		
+		return result;
+	}
+	
+	@RequestMapping("/station/update.action")
+	@ResponseBody
+	public Map<String,Object> updatestation(@RequestBody ServiceStation station, 
+			HttpServletRequest request,
+			Authentication authentication,
+            HttpServletResponse response) {
+		
+		//Result Map
+		Map<String,Object> result = new HashMap<String, Object>();
+
+		try{
+			stationService.edit(station, authentication);
+		}catch(Exception e){
+			result.put("message",e.getMessage());
+		}
+
+		
+		return result;
+	}
+	
+	@RequestMapping("/station/delete.action")
+	@ResponseBody
+	public Map<String,Object> deletestation(@RequestBody ServiceStation station, 
+			HttpServletRequest request,
+			Authentication authentication,
+            HttpServletResponse response) {
+		
+		//Result Map
+		Map<String,Object> result = new HashMap<String, Object>();
+		
+		try{
+			stationService.delete(station, authentication);
+		}catch(Exception e){
+			result.put("message",e.getMessage());
+		}
+
+		return result;
+	}
+	
 }
