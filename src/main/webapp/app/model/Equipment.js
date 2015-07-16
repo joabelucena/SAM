ea49dbc28761c24c0c5562661fe9c28cd6610e74
Joabe Lucena <jlucena@ttrans.com.br>
2015-07-16 15:55:00 -0300

    PROD :: ExtJs Translation

--- src/main/webapp/app/model/Equipment.js
+++ src/main/webapp/app/model/Equipment.js
@@ -1,27 +1,52 @@
 Ext.define('Sam.model.Equipment', {
 	extend: 'Ext.data.Model',
 
-	fields:[
+	fields:	[
 
-	        {name: 'id'					, type: 'string'														},
-			{name: 'fixed_asset'		, type: 'string'	, mapping: 'fixedAsset'								},
-			{name: 'service_tag'		, type: 'string'	, mapping: 'serviceTag'								},
-			{name: 'ip'					, type: 'string'														},
-			{name: 'type'				, type: 'string'	, mapping: 'type.desc'								},
-	        {name: 'model'				, type: 'string'	, mapping: 'model.desc'								},
-	        {name: 'manufacturer'		, type: 'string'	, mapping: 'manufacturer.desc'						},
-	        {name: 'site'				, type: 'string'	, mapping: 'site.desc'								},
-	        {name: 'counter_type'		, type: 'string'	, mapping: 'counter.desc'							},
-	        {name: 'system'				, type: 'string'	, mapping: 'system.desc'							},
-			{name: 'warranty'			, type: 'number'														},
-			{name: 'counter_qt'			, type: 'number'	, mapping: 'counterQt'								},
-			{name: 'oid'				, type: 'string'														},
-			{name: 'mtbf_prev'			, type: 'number'	, mapping: 'mtbfPrev'								},
-			{name: 'mtbf_calc'			, type: 'number'	, mapping: 'mtbfCalc'								},
-			{name: 'mtbf_manf'			, type: 'number'	, mapping: 'mtbfManf'								},
-			{name: 'install_date'		, type: 'date'		, mapping: 'installDate'		, dateFormat: 'time'},
-			{name: 'manufacture_date'	, type: 'date'		, mapping: 'manufactureDate'	, dateFormat: 'time'},
-			{name: 'acquired_date'		, type: 'date'		, mapping: 'acquiredDate'		, dateFormat: 'time'},
-			{name: 'remark'				, type: 'string'	, mapping: 'remark'									}
+		{name: 'id'					,type: 'string'	},
+		{name: 'fixed_asset'		,type: 'string'	},
+		{name: 'service_tag'		,type: 'string'	},
+		{name: 'ip'					,type: 'string'	},
+		{name: 'warranty'			,type: 'number'	},
+		{name: 'oid'				,type: 'string'	},
+		{name: 'mtbf_prev'			,type: 'number'	},
+		{name: 'mtbf_calc'			,type: 'number'	},
+		{name: 'mtbf_manf'			,type: 'number'	},
+		{name: 'install_date'		,type: 'date'	},
+		{name: 'manufacture_date'	,type: 'date'	},
+		{name: 'acquired_date'		,type: 'date'	},
+		{name: 'remark'				,type: 'string'	},
+
+		//Equipment Type Aux Fields
+		{name: 'type_id'			,type: 'number', mapping: 'type.id'		},
+		{name: 'type_desc'			,type: 'string', mapping: 'type.desc'	},
+		
+		//Equipment Model Aux Fields
+		{name: 'model_id'			,type: 'number', mapping: 'model.id'	},
+		{name: 'model_desc'			,type: 'string', mapping: 'model.desc'	},
+		
+		//Equipment Manufacturer Aux Fields
+		{name: 'manufacturer_id'	,type: 'number', mapping: 'manufacturer.id'		},		
+		{name: 'manufacturer_desc'	,type: 'string', mapping: 'manufacturer.desc'	},
+		
+		//Equipment Site Aux Fields
+		{name: 'site_id'			,type: 'number', mapping: 'site.id'		},
+		{name: 'site_desc'			,type: 'string', mapping: 'site.desc'	},
+		
+		//Equipment System Aux Fields
+		{name: 'system_id'			,type: 'number', mapping: 'system.id'	},		
+		{name: 'system_desc'		,type: 'string', mapping: 'system.desc'	}
+		
+	],
+
+	belongsTo: [
+
+		{model: 'Sam.model.EquipmentType'			,foreignKey: 'type_id'	},
+		{model: 'Sam.model.EquipmentModel'			,foreignKey: 'model_id'	},
+		{model: 'Sam.model.EquipmentManufacturer' 	,foreignKey: 'manufacturer_id' },
+		{model: 'Sam.model.Site' 					,foreignKey: 'site_id'	},
+		{model: 'Sam.model.SubSystem' 				,foreignKey: 'system_id'}
+
 	]
+
 });
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -6,9 +6,10 @@
 		<title>SAM Login</title>
 		
 		<link  href="<c:url value="/resources/packages/ext-theme-classic/build/resources/ext-theme-classic-all.css" />" rel="stylesheet">
-		<link  href="<c:url value="/resources/resources/css/login.css" />" rel="stylesheet">  
-		<script src="<c:url value="/resources/packages/ext-theme-classic/build/ext-theme-classic.js" />"></script>
+		<link  href="<c:url value="/resources/resources/css/login.css" />" rel="stylesheet">
 		<script src="<c:url value="/resources/ext/ext-all.js" />"></script>
+		<script src="<c:url value="/resources/packages/ext-locale/build/ext-locale-pt_BR.js" />"></script>	   
+		<script src="<c:url value="/resources/packages/ext-theme-classic/build/ext-theme-classic.js" />"></script>
 		<script src="<c:url value="/app/login.js" />"></script>
 	</head>
 	<body></body>
