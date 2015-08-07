edcdd8d84394e91bfd973cc7fa37228e13fb6e6b
Joabe Lucena <jlucena@ttrans.com.br>
2015-08-07 15:25:23 -0300

    PROD :: Log Issue 404 bug fixed

--- src/main/webapp/app/store/ServiceOrderLog.js
+++ src/main/webapp/app/store/ServiceOrderLog.js
@@ -1,20 +1,18 @@
 Ext.define('Sam.store.ServiceOrderLog', {
 	extend: 'Ext.data.Store',
 	
-	requires: ['Sam.model.ServiceOrderLog'],
-	
 	model: 'Sam.model.ServiceOrderLog',
 	
 	autoLoad: true,
 	async: true,
 
-//	proxy: {
-//		type: 'ajax',
-//		url: 'so/load/log',
-//		reader: {
-//			type: 'json',
-//			root: 'data'
-//		}
-//	},
+	proxy: {
+		type: 'ajax',
+		url: 'so/load/log',
+		reader: {
+			type: 'json',
+			root: 'data'
+		}
+	},
 	
 });
