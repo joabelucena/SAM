6921e8f38e7efe0afbeba7da77fc3c59e3d91846
Joabe Lucena <joabelucena@gmail.com>
2016-03-28 11:18:07 -0300

    TEST :: Maestro Implementation

--- src/main/java/br/com/ttrans/samapp/dao/EventDao.java
+++ src/main/java/br/com/ttrans/samapp/dao/EventDao.java
@@ -14,15 +14,24 @@ public interface EventDao {
 	public void add(Event event);
 	public void edit(Event event, Authentication authentication);
 	public void delete(Event event, Authentication authentication);
+	
 	public int recognize(Long[] ids, Authentication authentication);
+	public void recognize(String extId, String user);
+	
 	public int normalize(Long id, Authentication authentication);
 	public void normalize(List<String> alarmsId, String equipment, String user);
+	public void normalize(String extId, String user);
+	
 	public int countByAlarm(Equipment equipment, Alarm alarm, Date date);
 	public int countByType(Equipment equipment, AlarmType type, Date date);
+	
 	public List<Long> activeAlarms(Equipment equipment, Alarm alarm);
 	public int activeAlarms();
 	public boolean isFiltered(Event event);
+	
 	public Event get(long id);
+	public Event get(String extId);
+	
 	public List<Event> loadData();
 	public List<String[]> loadData(int start, int limit);
 }
