8d0550dd21b312b20482381686000fe031f7ce46
Joabe Lucena <joabelucena@gmail.com>
2016-08-17 14:27:16 -0300

    PROD :: new AutoStore component for intercepting server messages created

--- src/main/webapp/app/lib/AutoStore.js
+++ src/main/webapp/app/lib/AutoStore.js
@@ -1,92 +1,64 @@
 Ext.define('Sam.lib.AutoStore', {
 	extend: 'Ext.data.Store',
-    
-    alias: 'widget.autostore',
- 
-    constructor: function(config) {
-        this.callParent(arguments);
-    },
-    
-    proxy: {
-        type: 'ajax',
+	
+	autoLoad: false,
+	
+	constructor: function(config) {
+		this.callParent([config]);
         
+		this.proxy.on('exception', this.onProxyException, this);
+    },
+	
+	proxy: {
+		type: 'ajax',
+		
+		enablePaging: false,
+    	
+		simpleSortMode: true,
+		
         reader: {
             type: 'json',
-            successProperty: 'success',
             root: 'data',
+            totalProperty: 'total',
+            successProperty: 'success',
             messageProperty: 'message'
         },
-        
         writer: {
-            type: 'associatedjson',
+        	type: 'associatedjson',
             writeAllFields: true,
         },
         
-        listeners: {
-        	
-            exception: function(proxy, response, operation){
-            	var message, json;
-    	    	
-            	try {
-            		
-            		//Parseia Retorno
-            		json = Ext.JSON.decode(operation._response.responseText);
-                		
-            		message = json.message; 
-    
-            	}
-            	catch (ex) {
-            		message = 'Problemas na requisição, favor entrar em contato com o Adminsitrador do sistema';
-            	}
-                	
-            	//Exibir Mensagem
-            	Ext.MessageBox.show({
-            		title: 'Falha na Requisição',
-            		msg: message,
-            		buttons: Ext.MessageBox.OK,
-            		icon: Ext.MessageBox.WARNING
-            	});
-            
-            },
-        }
-        	
-    },
-    
-    listeners: {
-    	 
-    	    write: function(store, operation, eOpts) {
-    	    	var message, json;
-    	    	
-    	    	if(!operation.success){
-    	    		try {
-                		
-                		//Parseia Retorno
-    	    			json = Ext.JSON.decode(operation._response.responseText);
-                		
-    	    			message = json.message; 
-    
-                    }
-                    catch (ex) {
-                    	message = 'Problemas na requisição, favor entrar em contato com o Adminsitrador do sistema';
-                    }
-                	
-                    //Exibir Mensagem
-                	Ext.MessageBox.show({
-    			        title: 'Falha na Requisição',
-    			        msg: ErrorMessage,
-    			        buttons: Ext.MessageBox.OK,
-    			        icon: Ext.MessageBox.WARNING
-    				});
-    	    	}
-    	    	
-    	    },
-    	    load: function(a, e, i, o, u) {
-    	     	console.log("load");
-    	    },
-    	    read: function(a, e, i, o, u) {
-    	     	console.log("read");
-    	    }
-    }
-    
+	},
+	
+	onProxyException: function(proxy, response, operation, eOpts) {
+		 this.rejectChanges();
+		 
+		/*
+     	 * Runs when server returns 'success: false'
+     	 */
+     	var message, json;
+	    	
+     	try {
+     		
+     		//Parse returning message
+     		json = Ext.JSON.decode(operation._response.responseText);
+         		
+     		message = json.message; 
 
+     	}
+     	catch (ex) {
+     		message = 'Problemas na requisição, favor entrar em contato com o administrador do sistema';
+     	}
+         	
+     	//Exibir Mensagem
+     	Ext.MessageBox.show({
+     		title: 'Falha na Requisição',
+     		msg: message,
+     		buttons: Ext.MessageBox.OK,
+     		icon: Ext.MessageBox.WARNING
+     	});
+     
+     
+		 
+	 },
 });
