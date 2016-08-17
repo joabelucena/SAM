77482cf085b0ef2997e317624a134e38faa37315
Joabe Lucena <joabelucena@gmail.com>
2016-08-17 14:53:35 -0300

    PROD :: Server Firebird MessageHandler

--- src/main/java/br/com/ttrans/samapp/validator/ErrorMessageHandler.java
+++ src/main/java/br/com/ttrans/samapp/validator/ErrorMessageHandler.java
@@ -2,7 +2,10 @@ package br.com.ttrans.samapp.validator;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
+import org.firebirdsql.jdbc.FBSQLException;
+import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
@@ -26,5 +29,80 @@ public class ErrorMessageHandler {
 		}
 		
 		return result;
+	}
+	
+	public static void getUserMessage(Exception e, Map<String, Object> result) {
+		
+		String message ;
+		
+		if(e.getCause() instanceof FBSQLException){
+			
+			/*
+			 * Firebird exception handling
+			 */
+			FBSQLException ex = (FBSQLException) e.getCause();
+			
+			switch (ex.getErrorCode()) {
+			
+			case 335544665:
+				/*
+				 * Unique key or primary key violation
+				 */
+				message = "Registro duplicado. Verifique as informações digitadas e tente novamente.";
+				break;
+				
+			case 335544558:
+				/*
+				 * User constraint violation
+				 */
+				message = "Falha ao inserir registro. As informação solicitada não pode ser inserida devido a algumas regras do sistema.";
+				break;
+				
+			case 335544466:
+				/*
+				 * Foreign key violation
+				 */
+				message = "Cadastre primeiro todas as informações necessárias vinculadas a esse cadastro e tente novamente.";
+				break;
+
+			default:
+				message = "Problemas na requisição, favor entrar em contato com o administrador do sistema.";
+				break;
+			}
+			
+			
+		} else if(e.getCause() instanceof DataException){
+			
+			/*
+			 * Hibernate exception handling
+			 */
+			DataException ex = (DataException) e.getCause();
+			
+			switch (ex.getSQLState()) {
+			
+			case "22001":
+				/*
+				 * Data truncation
+				 */
+				message = "A quantidade de caracteres excede o permitido.";
+				break;
+
+			default:
+				message = "Problemas na requisição, favor entrar em contato com o administrador do sistema.";
+				break;
+			}			
+			
+		
+		} else {
+			message = "Problemas na requisição, favor entrar em contato com o administrador do sistema.";
+		}
+
+		/*
+		 * Set exception parameters set on AutoStore.js file:
+		 * successProperty: 'success',
+         * messageProperty: 'message'
+		 */
+		result.put("success", false);
+		result.put("message", message);
 	}
 }
