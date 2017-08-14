015c6e35159a9a83a18c24c46043c3752cf83b53
Joabe Lucena <joabelucena@gmail.com>
2017-08-14 10:46:39 -0300

    TEST :: JasperReports working

--- src/main/java/br/com/ttrans/samapp/controller/ReportController.java
+++ src/main/java/br/com/ttrans/samapp/controller/ReportController.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URISyntaxException;
+import java.rmi.RemoteException;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.io.IOUtils;
@@ -15,16 +19,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.MapType;
 
 import br.com.ttrans.samapp.library.DAO;
+import it.eng.spagobi.sdk.documents.bo.SDKDocument;
+import it.eng.spagobi.sdk.proxy.DocumentsServiceProxy;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -53,163 +62,76 @@ public class ReportController {
 	
 	
 	
-//	@RequestMapping(value = "/setReportInfo", method = RequestMethod.POST)
-//	@ResponseStatus(value=HttpStatus.OK)
-//	public void spagoSetReport(HttpServletRequest request
-//			, @RequestParam(required=true, value="label") String label
-//			, Authentication aut
-//			, Locale locale
-//			, HttpServletResponse response){
-//		
-//		String user = dao.getMv("SYS_SPGUSER"	, "");
-//		String pass = dao.getMv("SYS_SPGPASS"	, "");
-//		String url =  dao.getMv("SYS_SPGURL"	, "");
-//		String role = dao.getMv("SYS_SPGROLE"	, "");
-//		String message = null;
-//		
-//		HttpSession session = request.getSession();
-//		
-//		//Retorna o documento 
-//		SDKDocument document = getDoc(user, pass, label ,message);
-//		
-//		//Encontrou Documento
-//		if(document != null){
-//			session.setAttribute("spagobi_user"			, user				);
-//			session.setAttribute("spagobi_pwd"			, pass				);
-//			session.setAttribute("spagobi_documentId"	, document.getId()	);
-//			session.setAttribute("spagobi_role"			, role				);
-//			session.setAttribute("spagobi_url"			, url				);
-//			
-//		}else{
-//			session.setAttribute("spagobi_userMessage", message);
-//		}
-//	}
-//	
-//	/**
-//	 * This function returns a Spago Document from a 'label' passed as parameter.
-//	 * @param user
-//	 * @param pass
-//	 * @param label
-//	 * @param message
-//	 * @return SDKDocument (SpagoBi Report)
-//	 */
-//	private SDKDocument getDoc(String user, String pass, String label, String message){
-//		
-//		SDKDocument document = null;	
-//		
-//		DocumentsServiceProxy proxy = new DocumentsServiceProxy(user, pass);
-//		
-//		String endpoint = dao.getMv("SYS_SPGEND"	, "http://10.27.0.1:8180/SpagoBI/sdk/DocumentsService");
-//		
-//		proxy.setEndpoint(endpoint);
-//		
-//		try {
-//			
-//			SDKDocument[] documents = proxy.getDocumentsAsList(null, null, null);
-//			
-//			for(int i = 0; i < documents.length ; i ++ ){
-//				if(documents[i].getLabel().equals(label)) document = documents[i];
-//			}
-//
-//			
-//		} catch (RemoteException e) {
-//			message = "Ocorreram erros na execução do relatório. Entre em contato com o administrador do sistema";
-//			logger.error(e.getMessage());			
-//		}
-//		
-//		return document;
-//	}
-
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/jasper", produces={"application/pdf"})
-	public void getRpt1(
-			@RequestParam(required=true, value="label") String label,
-//			@RequestBody Map<String, Object> params,
-			HttpServletResponse response) throws JRException, IOException {
-	
-//	public void getRpt1(HttpServletResponse response) throws JRException, IOException {
+	@RequestMapping(value = "/setReportInfo", method = RequestMethod.POST)
+	@ResponseStatus(value=HttpStatus.OK)
+	public void spagoSetReport(HttpServletRequest request
+			, @RequestParam(required=true, value="label") String label
+			, Authentication aut
+			, Locale locale
+			, HttpServletResponse response){
 		
-//		System.out.println("chegou");
-//		String label = "RPT01";
+		String user = dao.getMv("SYS_SPGUSER"	, "");
+		String pass = dao.getMv("SYS_SPGPASS"	, "");
+		String url =  dao.getMv("SYS_SPGURL"	, "");
+		String role = dao.getMv("SYS_SPGROLE"	, "");
+		String message = null;
 		
-		logger.info("Path: " + (REPORTS_PATH + label + ".jasper"));
+		HttpSession session = request.getSession();
 		
-		BasicDataSource ds = (BasicDataSource) appContext.getBean("dataSource");
+		//Retorna o documento 
+		SDKDocument document = getDoc(user, pass, label ,message);
 		
-		try {
-			InputStream jasperStream = this.getClass().getResourceAsStream(REPORTS_PATH + label + ".jasper");
+		//Encontrou Documento
+		if(document != null){
+			session.setAttribute("spagobi_user"			, user				);
+			session.setAttribute("spagobi_pwd"			, pass				);
+			session.setAttribute("spagobi_documentId"	, document.getId()	);
+			session.setAttribute("spagobi_role"			, role				);
+			session.setAttribute("spagobi_url"			, url				);
 			
-			Map<String, Object> params = new HashMap<>();
-			
-			
-			params.put("logo_ttrans", "/home/joabe/Documents/workspace_jasper/SAM_REPORTS/img/TTRANS.png");
-
-//			params.put("Cartao de"			, "0");
-//			params.put("Cartao ate"			, "999");
-
-//			params.put("Nome de"			, "A");
-//			params.put("Nome ate"			, "Z");
-
-//			params.put("Tipo Cartao de"		, "VT");
-//			params.put("Tipo Cartao ate"	, "VT");
-			
-			params.put("EquDe", "TAG-001-002");
-			params.put("EquAte", "TAG-001-010");
-			
-			
-//			JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
-			
-			JasperReport jasperReport = JasperCompileManager.compileReport(this.getClass().getResourceAsStream(REPORTS_PATH + label + ".jrxml"));
-			
-			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, ds.getConnection());
-			
-//			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
-
-			response.setContentType("application/pdf; charset=UTF-8");
-			response.setHeader("Content-disposition", "inline; filename=" + label + ".pdf");
-//			response.setHeader("Content-Length", "999999");
-			
-			
-			
-
-			OutputStream outStream = response.getOutputStream();
-
-			
-//			JasperRunManager.runReportToPdfStream(jasperStream, outStream, params, ds.getConnection());
-//			JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
-			
-			SimplePdfExporterConfiguration conf = new SimplePdfExporterConfiguration();
-			
-			
-			conf.setPdfVersion(PdfVersionEnum.VERSION_1_7);
-			
-			
-			JRPdfExporter exporter = new JRPdfExporter();
-			
-			
-			
-			exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-			exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outStream));
-//			exporter.set
-			
-			
-			exporter.setConfiguration(conf);
-			
-//			exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
-//			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, outStream);
-//			exporter.setParameter(JRPdfExporterParameter.PDF_VERSION, JRPdfExporterParameter.PDF_VERSION_1_7);
-			
-			exporter.exportReport();
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		}else{
+			session.setAttribute("spagobi_userMessage", message);
 		}
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/jasperget", produces={"application/pdf"})
-	public void getRpt2(
-//			@RequestParam(required=true, value="label") String label,
+	/**
+	 * This function returns a Spago Document from a 'label' passed as parameter.
+	 * @param user
+	 * @param pass
+	 * @param label
+	 * @param message
+	 * @return SDKDocument (SpagoBi Report)
+	 */
+	private SDKDocument getDoc(String user, String pass, String label, String message){
+		
+		SDKDocument document = null;	
+		
+		DocumentsServiceProxy proxy = new DocumentsServiceProxy(user, pass);
+		
+		String endpoint = dao.getMv("SYS_SPGEND"	, "http://10.27.0.1:8180/SpagoBI/sdk/DocumentsService");
+		
+		proxy.setEndpoint(endpoint);
+		
+		try {
+			
+			SDKDocument[] documents = proxy.getDocumentsAsList(null, null, null);
+			
+			for(int i = 0; i < documents.length ; i ++ ){
+				if(documents[i].getLabel().equals(label)) document = documents[i];
+			}
+
+			
+		} catch (RemoteException e) {
+			message = "Ocorreram erros na execução do relatório. Entre em contato com o administrador do sistema";
+			logger.error(e.getMessage());			
+		}
+		
+		return document;
+	}
+
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/getReport", produces={"application/pdf"})
+	public void getReport(
 			@RequestParam Map<String, Object> params,
 			HttpServletResponse response) throws JRException, IOException, URISyntaxException {
 		
--- src/main/webapp/app/view/reports/jasper/JasperReports.js
+++ src/main/webapp/app/view/reports/jasper/JasperReports.js
@@ -1,112 +1,68 @@
 Ext.define('Sam.view.reports.jasper.JasperReports', {
-	extend: 'Ext.Panel',
-	
-	alias:  'widget.jasper',
-	
-	xUtils: Ext.create('Sam.lib.Util'),
-	
-	layout: {
-        type: 'fit'
-    },
-	
-	/***** Ajax Request Before Render *****/
-	listeners: {
-		beforerender:  function(me,eOpts) {
-			
-			
+	extend : 'Ext.Panel',
+
+	alias : 'widget.jasper',
+
+	xUtils : Ext.create('Sam.lib.Util'),
+
+	layout : {
+		type : 'fit'
+	},
+
+	/** *** Ajax Request Before Render **** */
+	listeners : {
+		beforerender : function(me, eOpts) {
+			console.log('inicio');
+
 			var data = this.xReportData;
 			
-//			var data = {
-//						"label"			: label	,
-//						"EquDe"			: "TAG-001-001"	,
-//						"EquAte"		: "TAG-001-002"	
-//						}
-			
-			
-			var rpt = {
-					xtype: 'container',
-					layout: 'fit',
-					autoEl: {
-						tag: 'iframe',
-						style: 'border: 0px; height:100% ; width:100%'
-					}
-			}
-			
-			
-			console.log('render')
+//			var iframeContainer = new Ext.BoxComponent({
+//			    autoEl: {
+//			        tag: 'iframe',
+//			        frameborder: '0',
+//			        src: 'http://wikipedia.org'
+//			    },
+//			    
+//			    
+//			    listeners: {
+//			        afterrender: function () {
+//			            console.log('rendered');
+//
+//			            this.getEl().on('load', function () {
+//			                console.log('loaded');
+//			            });
+//			        }
+//			    }
+//			});
+//			
+//			me.add(iframeContainer);
+
 			me.add({
-				xtype: 'container',
-				layout: 'fit',
-//				html: htmlText,
+				xtype : 'container',
+				layout : 'fit',
 				
-//				autoEl: {
-//					tag: 'embed',
-//					src: srcPDF,
-//					type: 'application/pdf',
-//					style: 'border: 0px; height:100% ; width:100%'
-//				}
-			
-				autoEl: {
-					tag: 'object',
-					data: 'rpt/jasperget' + this.xUtils.encodeURL(data),
-					type: 'application/pdf',
-					style: 'border: 0px; height:100% ; width:100%'
-				}
-			
+				autoEl : {
+					tag : 'object',
+					data : 'rpt/getReport' + this.xUtils.encodeURL(data),
+					type : 'application/pdf',
+					style : 'border: 0px; height:100% ; width:100%'
+				},
+				listeners: {
+			        afterrender: function () {
+			            console.log('LALALAL');
+
+			            this.getEl().on('load', function () {
+			                console.log('loaded');
+			            });
+			        }
+			    }
+
 			});
-			
+
 			me.autoRender = true;
-			
-//			Ext.Ajax.request({
-//	    		url : 'rpt/jasper',
-//	    		method : 'POST',
-//	    		
-//	    		params: {
-//	    			label: label
-//	    		},
-//	    		
-//	    		jsonData: data,
-//	
-//	    		success: function (result, request) {
-//	    			
-//	    			var htmlText = '<embed width=100% height=100%'
-//	                    + ' type="application/pdf"'
-//	                    + ' alt="pdf"' 
-//	                    + ' src="data:application/pdf;inline,'
-//	                    +  escape(result.responseText)
-//	                    + '"></embed>';
-//	    			
-//	    			var srcPDF = 'data:application/pdf; inline,'+ escape(result.responseText);
-//	    			console.log(result.responseText);
-//	    			me.add({
-//						xtype: 'container',
-//						layout: 'fit',
-////						html: htmlText,
-//						
-//						autoEl: {
-//							tag: 'embed',
-//							src: srcPDF,
-//							type: 'application/pdf',
-//							headers: 'Transfer-Encoding: chunked',
-//							style: 'border: 0px; height:100% ; width:100%'
-//						}
-//	    			
-////						autoEl: {
-////							tag: 'object',
-////							data: srcPDF,
-////							type: 'application/pdf',
-////							style: 'border: 0px; height:100% ; width:100%'
-////						}
-//	    			
-//	    			});
-//	    			
-//	    			me.autoRender = true;
-//	    		}	    			
-//	    	});
-			
-			
+
 		}
-			
+
 	}
-	
+
 });
